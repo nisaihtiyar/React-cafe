9bb59f4ae95f5637614ece46d30b93090d7398b6
Nisaİhtiyar <nisa.ihtiyar@bil.omu.edu.tr>
2024-02-02 21:24:07 +0300

    navbar

--- react-coffee/src/components/Navbar/Navbar.jsx
+++ react-coffee/src/components/Navbar/Navbar.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='bg-gardient-to-r form-secondary to-secondary/90 '>
+      <div className="container bg-red-500 ">
+        <div className="flex">
+          {/* logo section */}
+          <div>logo</div>
+
+          {/* links section */}
+          <div></div>
+
+        </div>
+      </div>
     </div>
   )
 }
